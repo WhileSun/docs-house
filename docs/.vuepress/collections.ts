d5a3d5123d2fa1b2075371d2d07b5f63fb96ab35
WhileSun <1207460050@qq.com>
2026-01-09 16:59:46 +0800

    vue生命周期

--- docs/.vuepress/collections.ts
+++ docs/.vuepress/collections.ts
@@ -16,12 +16,8 @@ export const frontend = defineCollection({
   title: '前端',
   sidebar: [
     // 次级 items 自动读取 typescript/guide 目录
-    {
-      text: 'vue', collapsed: false, items: [
-        { text: 'vue2', collapsed: false, prefix: 'vue2', items: 'auto' },
-        { text: 'vue3', collapsed: false, prefix: 'vue3', items: 'auto' },
-      ]
-    },
+    { text: 'vue2', collapsed: false, prefix: 'vue2', items: 'auto' },
+    { text: 'vue3', collapsed: false, prefix: 'vue3', items: 'auto' },
     { text: '小程序', prefix: 'miniprogram', items: 'auto' },
     { text: '开发工具', prefix: 'devtool', items: 'auto' },
   ],
